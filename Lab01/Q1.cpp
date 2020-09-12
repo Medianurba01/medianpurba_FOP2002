198e560ca92653e6f94f354a29a4e41cc4e53ecf
repl.it user <replituser@example.com>
2020-09-12 05:27:01 +0000

    hello world

---
+++ Lab01/Q1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main () {
+  //ini namain variabel
+  int a=12,b=5;
+  int penjumlahan, pengurangan, perkalian;
+  double pembagian, reminder;
+
+  //ini code untuk operasi
+  penjumlahan = a+b;
+  pengurangan = a-b;
+  perkalian = a*b;
+  pembagian = a/b;
+  reminder = a%b;
+
+  // ini mau ditampilkan di screen 
+  cout <<"12+5 = " << penjumlahan << endl;
+  cout <<"12-5 = " << pengurangan << endl;
+  cout <<"12*5 = " << perkalian << endl;
+  cout <<"12/5 = " <<pembagian<< endl;
+  cout <<"12%5 = " << reminder<< endl;
+}
