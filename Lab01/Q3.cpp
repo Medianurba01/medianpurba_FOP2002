045fbc68d803200a08c9908a659cd59c37cdb6ed
repl.it user <replituser@example.com>
2020-09-12 08:28:40 +0000

    code untuk jawaban 3

---
+++ Lab01/Q3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+  // nama variabel 
+  int a=7, b = 4;
+
+  printf ( "data sebelum proses pertukaran\n") ;
+  a=b, b=a ;
+
+  printf ("data setelah proses pertukaran\n") ;
+  return 0;
+
+}
