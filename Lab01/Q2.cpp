765459f054d6d877e985ac487042d9b2561a8e68
repl.it user <replituser@example.com>
2020-09-12 06:33:11 +0000

    code untuk jawaban 2

---
+++ Lab01/Q2.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+int main (){
+  // nama variabel 
+  int diameter = 10;
+  double radius, luas, keliling;
+  double pi = 3.14;
+  // memasukkan rumus 
+  radius = diameter/2;
+  luas = pi*radius*radius;
+  keliling = 2*pi*radius;
+  // menampilkan hasil di screen
+  cout <<"radius="<< radius << endl;
+  cout <<"luas"= << luas << endl;
+  cout <<"keliling=" << keliling << endl;
+  return 0;
+}
