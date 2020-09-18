cde9dcf6bd799d45d106e9cf423d680d30e7d149
repl.it user <replituser@example.com>
2020-09-18 07:06:45 +0000

    assigment Lab02

--- Lab02/Q4.cpp
+++ Lab02/Q4.cpp
@@ -5,14 +5,14 @@ int main()
 {
   // memasukkan derajat dalam celcius 
   double celcius;
-  cout << " enter degree in celcius = ";
+  cout << " derajat dalam celcius = ";
   cin>> celcius;
 
   // memasukkan derajat fahrenheit
   double fahrenheit = ( celcius * 9/5) + 32;
 
   // hasil
-  cout <<" celcius after chage = " << fahrenheit << " fahrenheit degree" <<endl; 
+  cout <<" celcius setelah diubah = " << fahrenheit << " derajat fahrenheit " <<endl; 
 
   return 0;
 
